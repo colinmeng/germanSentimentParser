c1071d030ea2be48544118f2af417c2d81685c5b
Colin Meng <colin220998@gmail.com>
2022-05-02 14:11:16 +0200

    - changed test scenario

--- test/NutzereingabenTest.py
+++ test/NutzereingabenTest.py
@@ -12,25 +12,60 @@ import spacy
 
 #paths
 LEXICON_PATH = "lexicon//sentimentLexicon.json"
-LEXICON_MODE = "no neutral"
+LEXICON_MODES = ["min","max","mean","no neutral","polarity"]
 
 nlp = spacy.load("de_core_news_lg")
-lex = lexicon.Lexicon(LEXICON_PATH,LEXICON_MODE)
 
 nutzerEingaben = open("test//testData//Nutzereingaben.txt","r",encoding="UTF-8")
 resultLog = open("test//logs//documentLevel//Nutzereingaben_result.txt","w",encoding="UTF-8")
 
 lines = nutzerEingaben.readlines()
 
-for line in lines:
+for lexiconMode in LEXICON_MODES:
+    lex = lexicon.Lexicon(LEXICON_PATH,lexiconMode)
+    resultLog.write(f"Ergebnisse für Lexikon-Modus: {lexiconMode}\n\n")
+    resultLog.write(f"pol\tactual\t\tcorrect\tmissingWords\tmessage\n")
 
-    msg = line.strip()
+    correctClassifiedCount = 0
+    FP, TP, FN, TN = 0,0,0,0
+    totalCount = len(lines)
 
-    docSent = sa.getDocumentSentiment(msg,lex,nlp)
-    missingWords = sa.getMissingWords(msg,lex,nlp)
+    for line in lines:
 
-    resultLog.write(f"{docSent}\t{missingWords} \n")
-    resultLog.write(msg+"\n\n")
+        msg = line.split("\t")[0].strip()
+        polarity = int(line.split("\t")[1].strip())
+
+        docPolarity = sa.getDocumentPolarity(msg,lex,nlp)
+        missingWords = sa.getMissingWords(msg,lex,nlp)
+
+        if polarity == docPolarity:
+            correctClassifiedCount += 1
+
+        # for confusion matrix
+        if polarity == -1:  
+            if docPolarity == -1:
+                TN += 1
+            else:
+                FP += 1
+        
+        elif polarity == 1:
+            if docPolarity == 1:
+                TP += 1
+            else:
+                FN += 1
+            
+        resultLog.write(f"{docPolarity}\t{polarity}\t{docPolarity == polarity}\t{missingWords}\t{msg}\n")
+
+    resultLog.write(f"{correctClassifiedCount} von {totalCount} korrekt klassifiziert.\n\n")
+        
+    accuracy = (TN + TP) / (TN + FP + TP + FN)
+    precision = TP / (TP + FP)
+    recall = TP / (TP + FN)
+    f1 = 2* ((precision * recall)/(precision + recall))
+
+    resultLog.write("Confusion Matrix\n_______________________________________________________\n")
+    resultLog.write(f"FP\t{FP}\tFN\t{FN}\tTP\t{TP}\tTN\t{TN}\taccuracy\t{accuracy}\tprecision\t{precision}\trecall\t{recall}\tf1\t{f1}") 
+    resultLog.write("\n\n")
 
 
 
